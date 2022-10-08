6d9e80eb86cefb675fb102e7c824bd9de65a993a
unknown <red30645@gmail.com>
2022-10-08 21:59:27 +0500

    Message

--- GH.py
+++ GH.py
@@ -1,3 +1,11 @@
+class Car:
+    def __init__(self, colour='Black'):
+        self.colour = colour
+
+BMW = Car
 print('Hello World')
 name = input("Please< write your name")
 print(name)
+
+a = 5 * 5
+print(a)
